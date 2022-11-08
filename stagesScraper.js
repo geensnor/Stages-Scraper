aa91ae958df8945d9aeb4164bf60d6ddff42e5b3
Joris <jorisreijbroek@gmail.com>
2022-11-08 20:42:23 +0100

    fout afhandeling

--- stagesScraper.js
+++ stagesScraper.js
@@ -17,27 +17,31 @@ async function scrapeData(url) {
 
         listItems.each(function (idx, el) { //Loop trough table
             let stageDataRaw = $(el).text();
-            let stageDataSplitted = $(stageDataRaw.split("\n"));
+            if (stageDataRaw === "No data") {
+                console.log("No stages found")
+            }
+            else {
+                let stageDataSplitted = $(stageDataRaw.split("\n"));
+                if (stageDataSplitted[3].replace(/\t/g, "") != "Date" && stageDataSplitted[4].replace(/\t/g, "") != "-") {//Skip header and resting days
 
-            if (stageDataSplitted[3].replace(/\t/g, "") != "Date" && stageDataSplitted[4].replace(/\t/g, "") != "-") {//Skip header and resting days
+                    outputDate = DateTime.fromFormat(`${stageDataSplitted[3].replace(/\t/g, "")}.${year}`, "d.LLL.yyyy").toFormat('y-MM-dd'); //Reformatting date
 
-                outputDate = DateTime.fromFormat(`${stageDataSplitted[3].replace(/\t/g, "")}.${year}`, "d.LLL.yyyy").toFormat('y-MM-dd'); //Reformatting date
-                let stageObject = {
-                    "number": stageNumber,
-                    "route": stageDataSplitted[5].replace(/\t/g, "").replace("-", " - "),
-                    "date": outputDate,
-                    "status": "notStarted",
-                    "stageResults": '',
-                    "jerseyWearers": ''
+                    let stageObject = {
+                        "number": stageNumber,
+                        "route": stageDataSplitted[5].replace(/\t/g, "").replace("-", " - "),
+                        "date": outputDate,
+                        "status": "notStarted",
+                        "stageResults": '',
+                        "jerseyWearers": ''
+                    }
+                    let yamlString = yaml.dump(stageObject);
+                    fs.writeFileSync(`output/etappe${stageNumber}.yaml`, yamlString, 'utf8');
+                    stageNumber++;
                 }
-
-                let yamlString = yaml.dump(stageObject);
-                fs.writeFileSync(`output/etappe${stageNumber}.yaml`, yamlString, 'utf8');
-
-                stageNumber++;
             }
         });
-        console.log(stageNumber + " stages written in output directory");
+        if (stageNumber > 1)
+            console.log(stageNumber + " stages written in output directory");
 
     } catch (err) {
         console.error(err);
