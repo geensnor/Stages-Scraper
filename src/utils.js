04950162a5ee94779c8a302320940820a55184b1
Joris <jorisreijbroek@gmail.com>
2025-11-10 12:30:12 +0100

    headers die beter werken

--- src/utils.js
+++ src/utils.js
@@ -6,8 +6,7 @@ const path = require("path");
 
 const OUTPUT_BASE = path.resolve(__dirname, "../output");
 const DEFAULT_HEADERS = {
-  "User-Agent":
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
 };
 
 async function fetchHtml(url) {
